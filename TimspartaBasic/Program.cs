de16a9d8515f3a38edae2818b282002c4c56cc79
hwon <phw97123@naver.com>
2023-08-14 12:18:44 +0900

    과제 1-2

--- TimspartaBasic/Program.cs
+++ TimspartaBasic/Program.cs
@@ -46,11 +46,22 @@ namespace TimspartaBasic
             //Console.WriteLine("BMI 지수는 {0:N2} 입니다", BMI);  
 
             //1. 사용자로부터 입력 받기 
-            Console.Write("이름을 입력하세요 : ");
-            string name = Console.ReadLine();
-            Console.Write("나이를 입력하세요 : ");
-            int age = int.Parse(Console.ReadLine()); 
-            Console.WriteLine($"안녕하세요, {name}! 당신은 {age} 세 이군요."); 
+            //Console.Write("이름을 입력하세요 : ");
+            //string name = Console.ReadLine();
+            //Console.Write("나이를 입력하세요 : ");
+            //int age = int.Parse(Console.ReadLine()); 
+            //Console.WriteLine($"안녕하세요, {name}! 당신은 {age} 세 이군요.");
+
+            //2. 간단한 사칙연산 계산기 만들기 
+            Console.Write("첫번째 수를 입력하세요 : "); 
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("두번째 수를 입력하세요 : ");
+            int num2 = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine($"더하기 : {num1+num2}");
+            Console.WriteLine($"빼기 : {num1 - num2}");
+            Console.WriteLine($"곱하기 : {num1 * num2}");
+            Console.WriteLine($"나누기 : {(float)num1 / num2}");
         }
     }
 }
