b4a1cdf8769cfb425e4afef698454eafeeb6a0e2
hwon <phw97123@naver.com>
2023-08-14 12:29:58 +0900

    과제 1-3

--- TimspartaBasic/Program.cs
+++ TimspartaBasic/Program.cs
@@ -53,15 +53,21 @@ namespace TimspartaBasic
             //Console.WriteLine($"안녕하세요, {name}! 당신은 {age} 세 이군요.");
 
             //2. 간단한 사칙연산 계산기 만들기 
-            Console.Write("첫번째 수를 입력하세요 : "); 
-            int num1 = int.Parse(Console.ReadLine());
-            Console.Write("두번째 수를 입력하세요 : ");
-            int num2 = int.Parse(Console.ReadLine());  
+            //Console.Write("첫번째 수를 입력하세요 : "); 
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("두번째 수를 입력하세요 : ");
+            //int num2 = int.Parse(Console.ReadLine());  
 
-            Console.WriteLine($"더하기 : {num1+num2}");
-            Console.WriteLine($"빼기 : {num1 - num2}");
-            Console.WriteLine($"곱하기 : {num1 * num2}");
-            Console.WriteLine($"나누기 : {(float)num1 / num2}");
+            //Console.WriteLine($"더하기 : {num1+num2}");
+            //Console.WriteLine($"빼기 : {num1 - num2}");
+            //Console.WriteLine($"곱하기 : {num1 * num2}");
+            //Console.WriteLine($"나누기 : {(float)num1 / num2}");
+
+            //3. 온도 변환기 만들기 
+            Console.Write("섭씨 온도를 입력하세요: "); 
+            int celsius = int.Parse(Console.ReadLine());
+            int fahrenheit = (celsius * 9/5) + 32;
+            Console.WriteLine($"변환된 화씨 온도: {fahrenheit}"); 
         }
     }
 }
