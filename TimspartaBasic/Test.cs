ccdb89746faefbeb8781fd956e5e6f77af0c6aae
hwon <phw97123@naver.com>
2023-08-24 18:40:50 +0900

    4주차 과제 완료

---
+++ TimspartaBasic/Test.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimspartaBasic
+{
+    internal class Test
+    {
+        //int를 반환하고 매개변수 2개를 받는 함수를 저장할 델리게이트 선언 
+        //delegate int Calculate(int x, int y);
+
+        ////매개변수 2개를 받아 더한 값을 int로 반환하는 함수
+        //static int Add(int x, int y)
+        //{
+        //    return x + y;
+        //}
+
+        //static void Main()
+        //{
+        //    //델리게이트에 메서드 등록
+        //    Calculate calc = Add;
+
+        //    //일반 int 형 변수에 int형을 반환하고 매개변수 2개를 받는 델리게이트 사용 
+        //    int result = calc(3, 5);
+        //}
+
+        //델리게이트를 그냥 변수에서 사용 
+
+        //반환값이 없고 문자열을 매개변수로 받는 델리게이트 선언
+        //delegate void MyDelegate(string message);
+
+        //static void Method1(string message)
+        //{
+        //    Console.WriteLine("Method1 : " + message);
+        //}
+
+        //static void Method2(string message) 
+        //{ 
+        //    Console.WriteLine("Method2 : "+ message);
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    MyDelegate myDelegate = Method1;
+        //    myDelegate += Method2;
+
+        //    myDelegate("델리게이트 사용");
+
+        //    Console.ReadKey(); 
+        //}
+
+        //두개의 결과 그냥 Delegate 는 추가하고 사용한다 
+
+        //델리게이트 이벤트
+        //반환값이 없고 float를 매개변수로 받는 델리게이트 선언
+        //public delegate void EnemyAttackHandler(float damage);
+
+        //public class Enemy
+        //{
+        //    //만들었던 델리게이트를 event 키워드로 선언
+        //    public event EnemyAttackHandler? OnAttack;
+
+        //    public void Attack(float damage)
+        //    {
+        //        //이벤트 호출 
+        //        Console.WriteLine("enemy가 공격을 했습니다.");
+        //        Console.WriteLine("호출 준비");
+        //        OnAttack?.Invoke(damage);
+        //        Console.WriteLine("호출 끝");
+        //    }
+        //}
+
+        //public class Player
+        //{
+        //    public void TakeDamage(float damage)
+        //    {
+        //        Console.WriteLine("플레이어가 {0}의 데미지를 입었습니다.", damage);
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Enemy enemy = new Enemy();
+        //    Player player = new Player();
+
+        //    //enemy의 이벤트 델리게이트에 플레이어의 함수 추가
+        //    enemy.OnAttack += player.TakeDamage;
+
+        //    //공격하면 플레이어의 함수 호출됨 
+        //    enemy.Attack(10.0f);
+        //}
+
+        ////실행되면 호출해라, 추가로 다른 함수도 호출해라
+        ////델리게이트 
+        //// 델리게이트 선언
+        //public delegate void enemyattackhandler(float damage);
+
+        //// 적 클래스
+        //public class enemy
+        //{
+        //    // 공격 이벤트
+        //    public enemyattackhandler onattack;
+
+        //    public enemy()
+        //    {
+        //        onattack = attack;
+        //    }
+
+        //    // 적의 공격 메서드
+        //    public void attack(float damage)
+        //    {
+        //        console.writeline("enemy가 공격을 했습니다.");
+        //        console.writeline("호출 준비");
+        //        console.writeline("호출 끝");
+        //    }
+        //}
+
+        //// 플레이어 클래스
+        //public class player
+        //{
+        //    // 플레이어가 받은 데미지 처리 메서드
+        //    public void handledamage(float damage)
+        //    {
+        //        // 플레이어의 체력 감소 등의 처리 로직
+        //        console.writeline("플레이어가 {0}의 데미지를 입었습니다.", damage);
+        //    }
+        //}
+
+        //// 게임 실행
+        //static void main()
+        //{
+        //    // 적 객체 생성
+        //    enemy enemy = new enemy();
+
+        //    // 플레이어 객체 생성
+        //    player player = new player();
+
+        //    // 플레이어의 데미지 처리 메서드를 적의 공격 이벤트에 추가
+        //    enemy.onattack += player.handledamage;
+
+        //    enemy.onattack(10);
+        //}
+    }
+}
---
+++ TimspartaBasic/Week4_TextRPGcs.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static TimspartaBasic.Week4_TextRPGcs;
+
+namespace TimspartaBasic
+{
+    internal class Week4_TextRPGcs
+    {
+        public static void StringCenter(string str)
+        {
+            Console.WriteLine(String.Format($"{str}").PadLeft(50 - (30 - (str.Length / 2))));// 가운데정렬
+        }
+
+        public interface ICharacter
+        {
+            string Name { get; set; }
+            int Health { get; set; }
+            int AttackPower { get; set; }
+
+            int Attack { get; }
+            bool IsDead { get; }
+
+            void TakeDamge(int damage);
+        }
+
+        public class Warrior : ICharacter
+        {
+
+            Random random = new Random();
+
+            public string Name { get; set; }
+
+            public int Health { get; set; }
+
+            public int AttackPower { get; set; }
+
+            public int Attack => random.Next(AttackPower - 5, AttackPower + 5);
+            public bool IsDead => Health <= 0;
+
+            public Warrior(string name)
+            {
+                Name = name;
+                Health = 100;
+                AttackPower = 20;
+            }
+            public void TakeDamge(int damage)
+            {
+                Health -= damage;
+                StringCenter($"{Name}은(는) {damage}의 피해를 입었다");
+            }
+
+        }
+        public class Monster : ICharacter
+        {
+            Random random = new Random();
+            public string Name { get; set; }
+
+            public int Health { get; set; } = 100;
+
+            public int AttackPower { get; set; }
+
+            public int Attack => random.Next(AttackPower - 10, AttackPower + 3);
+            public bool IsDead => Health <= 0;
+
+            public Monster(string name)
+            {
+                Name = name;
+                AttackPower = 10;
+            }
+            public void TakeDamge(int damage)
+            {
+                Health -= damage;
+                StringCenter($"{Name}은(는) {damage}의 피해를 입었다");
+               
+            }
+        }
+
+        public interface IItem
+        {
+            string Name { get; set; }
+
+            int Amount { get; set; }
+
+            //미리 정의된 델리게이트로 값을 반환하지 않는 메서드를 나타내는 델리게이트 
+            Action Enforce { get; set; }
+
+            void Use(Warrior warrior);
+
+            void AmountUp(); 
+        }
+
+        public class HealthPotion : IItem
+        {
+            public string Name { get; set; } = "체력포션";
+
+            public int Amount { get; set; } = 30;
+            public void Use(Warrior warrior)
+            {
+                warrior.Health += Amount;
+                StringCenter($"{Name}을 마셨다");
+            }
+
+            public Action Enforce { get; set; }
+
+            public HealthPotion()
+            {
+                //델리게이트에 람다식으로 등록
+                Enforce = AmountUp; 
+            }
+
+            public void AmountUp()
+            {
+                if (Name != "강화체력포션")
+                {
+                    Amount += 30;
+                    StringCenter($"{Name} 아이템이 강화되었습니다.");
+                    Name = "강화체력포션";
+                }
+                else
+                {
+                    StringCenter($"강화{Name}은 존재하지 않네요");
+                }
+            }
+        }
+
+        public class StrengthPotion : IItem
+        {
+            public string Name { get; set; } = "공격력포션";
+            public int Amount { get; set; } = 30; 
+
+            public Action Enforce { get; set; }
+
+            public StrengthPotion()
+            {
+                Enforce = AmountUp;
+            }
+
+            public void Use(Warrior warrior)
+            {
+                warrior.AttackPower += Amount;
+                StringCenter($"{Name}을 마셨다");
+            }
+            public void AmountUp()
+            {
+                if (Name != "강화공격력포션")
+                {
+                    Amount += 30;
+                    StringCenter($"{Name} 아이템이 강화되었습니다.");
+                    Name = "강화공격력포션";
+                }
+                else
+                {
+                    StringCenter($"강화{Name}은 존재하지 않네요");
+                }
+            }
+        }
+
+        public class Stage
+        {
+            //델리게이트 선언 
+            public delegate void GameEvent(ICharacter character);
+            //델리게이트 이벤트 등록
+            public event GameEvent OnCharacterDeath;
+
+            ICharacter player;
+            ICharacter monster;
+            List<IItem> rewardsItem;
+
+            int level;
+
+            public Stage(ICharacter player, ICharacter monster, List<IItem> rewardsItem, int level)
+            {
+                this.player = player;
+                this.monster = monster;
+                this.rewardsItem = rewardsItem;
+                this.level = level;
+                OnCharacterDeath += StageClear; //캐릭터 죽으면 StageClear 실행
+            }
+
+            public void Start()
+            {
+                Console.Clear();
+                string stageStrMessge = $"스테이지{level} 시작!";
+                StringCenter(stageStrMessge);
+                Console.WriteLine("  ------------------------------------------------");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+
+                while (!player.IsDead && !monster.IsDead)
+                {
+                    Console.WriteLine($"{player.Name,8}({player.Health}) -> 공격!");
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+                    monster.TakeDamge(player.Attack);
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+
+                    if(monster.IsDead) { break; }
+
+                    Console.WriteLine($"공격! <- {monster.Name}({monster.Health})".PadLeft(46));
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+                    player.TakeDamge(monster.Attack);
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+                }
+
+                if (player.IsDead)
+                {
+                    Console.WriteLine();
+                    StringCenter($"{player.Name}이(가) 죽었습니다.");
+                    Thread.Sleep(1000);
+                    //StageClear(monster); 
+                    OnCharacterDeath?.Invoke(monster);
+                }
+                else if (monster.IsDead)
+                {
+                    Console.WriteLine();
+                    StringCenter($"{monster.Name}이(가) 죽었습니다.");
+                    Thread.Sleep(1000);
+                    //StageClear(player); 
+                    OnCharacterDeath?.Invoke(player);
+                }
+            }
+
+            void StageClear(ICharacter character)
+            {
+                if (character == player)
+                {
+                    Console.WriteLine("  ------------------------------------------------");
+                    StringCenter("스테이지 클리어!");
+                    Console.WriteLine(); 
+                    Console.WriteLine("      보상의 이름을 입력하여 선택하세요");
+                    Console.WriteLine(); 
+                    foreach (var item in rewardsItem)
+                    {
+                        StringCenter($"{item.Name}  ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("  ------------------------------------------------");
+                    Console.Write(">> "); 
+                    string? input = Console.ReadLine();
+
+
+                    IItem? selectedItem = rewardsItem.Find(item => item.Name == input);
+
+                    if (selectedItem != null)
+                    {
+                        //Warrior는 곧 player니까 형변환 가능
+                        Console.WriteLine(); 
+                        selectedItem.Use((Warrior)player);
+                        Console.WriteLine();
+                        selectedItem.Enforce();
+                        //물약 강화
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        StringCenter("아무것도 선택하지 않으셨습니다.");
+                        Thread.Sleep(1000);
+                    }
+                    Console.WriteLine();
+                    StringCenter("다음 스테이지로 넘어갑니다.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    StringCenter("YOU DIED");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public class GameManager
+        {
+            Warrior player;
+            Monster Slime;
+            Monster goblin;
+            Monster dragon;
+
+            List<IItem> stageRewards;
+
+            List<Stage> stageMap;
+
+            Stage stage1;
+            Stage stage2;
+            Stage stage3;
+
+            public GameManager()
+            {
+                player = new Warrior("Player");
+                Slime = new Monster("슬라임");
+                goblin = new Monster("고블린");
+                dragon = new Monster("드래곤");
+
+                stageRewards = new List<IItem> { new HealthPotion(), new StrengthPotion() };
+
+                stage1 = new Stage(player, Slime, stageRewards, 1);
+                stage2 = new Stage(player, goblin, stageRewards, 2);
+                stage3 = new Stage(player, dragon, stageRewards, 3);
+
+                stageMap = new List<Stage>();
+
+                stageMap.Add(stage1);
+                stageMap.Add(stage2);
+                stageMap.Add(stage3);
+            }
+
+            public void StageStart()
+            {
+                foreach (var stage in stageMap)
+                {
+                    string startMessage = "게임을 시작합니다.";
+                    StringCenter(startMessage); 
+                    Thread.Sleep(1000);
+
+                    stage.Start();
+
+                    if (player.IsDead) return;
+
+                    StringCenter("아무키나 눌러서 다음 스테이지 시작"); 
+                    Console.ReadKey(); 
+                }
+                StringCenter("Victory!!");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(53, 40);
+            Console.CursorVisible = false; 
+            GameManager Game = new GameManager();
+            Game.StageStart();
+
+            //stage1.StageStart();
+
+            //if (player.IsDead) return;
+
+
+
+            //stage2.StageStart();
+
+            //if(player.IsDead) return;
+
+            //Console.WriteLine("게임 끝!");
+        }
+    }
+}
+
+
+
